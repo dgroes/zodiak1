01cab948c650b83deea61793b2ce4a8b80156106
Diego Pasten <diegopasten78@gmail.com>
2025-06-20 16:05:23 -0400

    feat: :building_construction: Se corrigen erroes de tipado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            PeobleSeeder::class,
+            PeopleSeeder::class,
             StaffSeeder::class,
-            TowerSeeder::class,
             UserSeeder::class,
+            // TowerSeeder::class,
         ]);
     }
 }
