4469eff7ca71db48c89c9a4149e6eca23daede63
Diego Pasten <diegopasten78@gmail.com>
2025-06-28 17:23:14 -0400

    feat: :truck: Actualización de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Residents\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -8,8 +9,31 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view("log", 'log')
+    ->middleware(['auth', 'verified'])
+    ->name('log');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Otras rutas de tu aplicación...
+
+    /**
+     * Ruta para la gestión de residentes
+     *
+     * ¿Por qué usar Livewire::get()?
+     * - Simplifica la integración de componentes Livewire
+     * - Maneja automáticamente el montaje del componente
+     * - Permite usar middleware de autenticación
+     */
+    Route::get('/residents', Index::class)
+        ->name('residents.index')
+        ->middleware('can:viewAny,App\Models\Resident');
+});
+
+
+
+require __DIR__ . '/auth.php';
