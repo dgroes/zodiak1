fe05bc12959b1b69066acb5ddedfc80b312815c6
Diego Pasten <diegopasten78@gmail.com>
2025-06-28 17:23:52 -0400

    feat: :construction_worker: Relación de modelos: Apartment y Resident

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -3,8 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Apartment extends Model
 {
-    //
+    protected $fillable = [
+        'owner_id',
+        'tower_id',
+        'number',
+        'details',
+        'is_available'
+    ];
+
+    //Relación con el propietario 🔑
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    // Relación con la Torre 🏢
+    public function tower(): BelongsTo
+    {
+        return $this->belongsTo(Tower::class);
+    }
+
+    /*
+        Relación con los residentes 👨‍👩‍👦‍👦
+        Un departamento puede tener muchos residentes
+    */
+    public function residents(): HasMany
+    {
+        return $this->hasMany(Resident::class);
+    }
 }
--- app/Models/Resident.php
+++ app/Models/Resident.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Resident extends Model
 {
-    //
+    // $fillable <-- Protege contra asignación masiva
+    protected $fillable = [
+        'run',
+        'document_number',
+        'names',
+        'last_names',
+        'apartment_id',
+        'medical_disabilities'
+    ];
+
+    /*
+        Relación con el depto (apartment)
+        Un residente pertenece a un departamento
+    */
+
+    //Con belongsTo se define que un residente pertenece a un departamento
+    public function apartment(): BelongsTo
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
